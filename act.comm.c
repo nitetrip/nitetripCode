944d91f8ae2384b23fe51a9b5050951493914fda
Sorinmud <nitetrip@gmail.com>
2012-03-05 18:55:34 -0400

    test change seeing how git works

--- act.comm.c
+++ act.comm.c
@@ -7,6 +7,7 @@
 *  Copyright (C) 1993, 94 by the Trustees of the Johns Hopkins University *
 *  CircleMUD is based on DikuMUD, Copyright (C) 1990, 1991.               *
 ************************************************************************ */
+//  a test for git
 
 #include "conf.h"
 #include "sysdep.h"
