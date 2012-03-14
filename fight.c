bcdbf1d60ebb653692ba144f53c14c66a7fd8aee
sorin.kelp <kelp@titus.(none)>
2012-03-14 15:05:56 -0300

    git testing

--- fight.c
+++ fight.c
@@ -7,6 +7,7 @@
 *  Copyright (C) 1993, 94 by the Trustees of the Johns Hopkins University *
 *  CircleMUD is based on DikuMUD, Copyright (C) 1990, 1991.               *
 ************************************************************************ */
+// testing with a comment here - kelp
 
 #include "conf.h"
 #include "sysdep.h"
